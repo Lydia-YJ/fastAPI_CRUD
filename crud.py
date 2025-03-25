ef82e06bee6aaf7075a3ac442261ebea6b5ae9d5
한유정 <dbwjdcheld@naver.com>
2025-03-25 12:24:55 +0000

    finish crud

--- crud.py
+++ crud.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from dotenv import load_dotenv
-import pymysql
 import os
-
-app = FastAPI()
+import redis
+import pymysql
 
 load_dotenv() 
+app = FastAPI()
 
 timeout = 10
 connection = pymysql.connect(
@@ -18,9 +19,34 @@ connection = pymysql.connect(
   read_timeout=timeout,
   port=11559,
   user="avnadmin",
-  write_timeout=timeout,
-)
-  
+  write_timeout=timeout,)
+
+r = redis.Redis(
+    host='redis-18340.c8.us-east-1-4.ec2.redns.redis-cloud.com',
+    port=18340,
+    decode_responses=True,
+    username="default",
+    password="4OlmdlfV6PaC766AZhiRMXi6WM3qJTze")
+
+class Article(BaseModel):
+    title: str
+    body: str
+
+# success = r.set('foo', 'bar')
+# True
+
+# TODO: read from cache (hint) = 캐시(cache)에서 데이터를 읽는 기능을 구현해야 한다
+# result = r.get('foo')
+# print(result) # >>> bar
+
+@app.post("/articles")
+def write_article(article:Article):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO articles (title, body) VALUES('{article.title}', '{article.body}')")
+    finally:
+        pass
+
 @app.get("/articles")
 def read_articles():
   try:
@@ -32,3 +58,63 @@ def read_articles():
 
   finally:
       pass
+
+# 기본 데이터 읽어오기
+# @app.get("/articles/{id}")
+# def read_articles(id):
+#   try:
+#       cursor = connection.cursor()
+#       cursor.execute(f"SELECT * FROM articles WHERE id={id}")
+#       result = cursor.fetchall()[0] #DB API 에서 쿼리 결과를 리스트 형태로 반환
+#       print(result)
+#       #print(result[-1]) 하나의 데이터를 굳이 리스트로 출력 x
+#       return result
+#   finally:
+#       pass
+
+# 캐시(cache)로 데이터 읽어오기
+@app.get("/articles/{id}")
+def read_articles(id):
+  try:
+      cached = r.hgetall(id)
+      print('c', cached)
+      if cached == {}:
+        print("first") # 만약 빈 딕셔너리라면 {} 처음 조회라 데이터가 없다는 뜻
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM articles WHERE id={id}")
+        result = cursor.fetchall()[0]
+        # print(result['title'], result['body'])
+        r.hmset(id, result)
+        return result
+      else:
+         print("cached") # 캐시됨
+         return cached
+  finally:
+      pass
+  
+@app.put("/articles/{id}")
+def update_articles(id, article:Article):
+   #DB
+   title = article.title
+   body = article.body
+   try:
+      cursor = connection.cursor()
+      query = f"UPDATE articles SET title='{article.title}', body='{article.body}' WHERE id={id}"
+      cursor.execute(query)
+   finally:
+      pass
+   
+   #cache
+   r.hmset(id, {'id':id, 'title':title, 'body':body})
+
+@app.delete("/articles/{id}")
+def delete_article(id):
+    try:
+        cursor = connection.cursor()
+        query = f"DELETE FROM articles WHERE id={id}"
+        cursor.execute(query)
+    finally:
+        pass
+
+    # Cache
+    r.delete(id)
